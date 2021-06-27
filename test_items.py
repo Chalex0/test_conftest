b744e14c2d66afd3625995985e9b51e19b2dcc1d
Александр Черников <Chalex0@gmail.com>
2021-06-27 20:07:25 +0300

    Stepik - тестирование конфигурации в conftest.py 27.06.2021

--- test_items.py
+++ test_items.py
@@ -10,5 +10,6 @@ def test_guest_should_see_button_add_to_basket(browser):
     wait = WebDriverWait(browser, 5) 
     browser.get(link)
     button = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[contains(@class, "btn-add-to-basket")]')))
+    assert button.get_attribute('type') == 'submit', 'Кнопка отсутствует на странице'
     button.click()
  
