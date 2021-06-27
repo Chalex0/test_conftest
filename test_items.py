2e4731750fb35bf0a9e1c646c5fd7fdfb634a62a
Александр Черников <Chalex0@gmail.com>
2021-06-27 19:00:31 +0300

    Stepik - тестирование конфигурации в conftest.py 27.06.2021

---
+++ test_items.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+def test_guest_should_see_button_add_to_basket(browser):
+    wait = WebDriverWait(browser, 5) 
+    browser.get(link)
+    button = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[contains(@class, "btn-add-to-basket")]')))
+    button.click()
+ 
